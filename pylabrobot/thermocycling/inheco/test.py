5dd7ea671e6401d2122ca4c95c2adcffc72b8b6e
rkaptur <r.kaptur@outlook.com>
2025-11-08 22:59:56 +0100

    feat(inheco): working response from the event receiver

--- pylabrobot/thermocycling/inheco/test.py
+++ pylabrobot/thermocycling/inheco/test.py
@@ -1,24 +1,23 @@
 from tc import InhecoPMSClient, SiLAReturnValue
-from tc import ThermoCycler
 
+# Call GetParameters on the thermocycler and print results.
 with InhecoPMSClient(endpoint="http://169.254.173.35:7071/ihc") as client:
-    rv = SiLAReturnValue(return_code=1, message="Python client test", device_class=30)
-    resp = client.status_event(
-        device_id="python-client",
+    rv = SiLAReturnValue(return_code=1, message="Python client test")
+    response_payload = (
+        "<ResponseData>"
+        "<ParameterSet>"
+        '<Parameter name="Message">'
+        "<String>Hello from Python client</String>"
+        "</Parameter>"
+        "</ParameterSet>"
+        "</ResponseData>"
+    )
+    resp = client.response_event(
+        request_id=1,
         return_value=rv,
-        event_description="Connectivity check from tc.ipynb",
+        response_data=response_payload,
     )
     print("Sent envelope:")
     print(client._client._last_envelope.decode("utf-8"))
     print("Response:")
     print(resp)
-
-    # Call GetParameters on the thermocycler (SOAP interface) and print results.
-    tc = ThermoCycler(ip="169.254.173.35")
-    params = tc.get_parameters(
-        pms_client=client,
-        send_status_event=True,
-        status_event_description="GetParameters hook from test.py",
-    )
-    print("Parsed ThermoCyclerParameters:")
-    print(params)
